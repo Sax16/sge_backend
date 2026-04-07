b7e43f5b9b5de8f790a615e71f9d902f8fbd4efb
jhorsax161616 <71660993@continental.edu.pe>
2026-04-07 02:11:43 -0500

    Implementando el endpoint para obtener al usuario logueado

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from app.dependencies import get_db
+from app.dependencies import get_db, get_current_user
 from app.services.auth_service import authenticate_user
 from app.core.security import create_access_token
 from app.schemas.token import Token
+from app.schemas.user import UserRead
 
 router = APIRouter()
 
@@ -22,3 +23,10 @@ async def login(
         )
     access_token = create_access_token(data={"sub": user.user_name})
     return {"access_token": access_token, "token_type": "Bearer"}
+
+
+@router.get("/login/me", response_model=UserRead)
+async def read_users_me(
+    current_user: UserRead = Depends(get_current_user),
+) -> UserRead:
+    return current_user
